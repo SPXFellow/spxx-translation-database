c29f3f48a7acc8e073ee3fe477eba5e8d9826f12
RicoloveFeng <44725122+RicoloveFeng@users.noreply.github.com>
2022-05-13 16:49:02 +0800

    change color supplier

--- check_translations.py
+++ check_translations.py
@@ -56,11 +56,11 @@ if __name__ == "__main__":
     with open("README.md", "w") as f:
         def make_table(data: dict, color: dict) -> str:
             if data:
-                header = "|Translator|Score|Color|"
-                splitter = "|---|---|---|"
+                header = "|Translator|Score|"
+                splitter = "|---|---|"
                 strs = [header, splitter]
                 for tr, score in sorted([(tr, score) for tr, score in data.items()], key=lambda x: x[1], reverse=True):
-                    strs.append("|{}|{}|{}|".format(tr, score,"![#{c}](https://via.placeholder.com/15/{c}/000000?text=+)".format(c = color[tr][1:] if tr in color else "388d40")))
+                    strs.append("|{}|{}|".format("![#{c}](https://img.shields.io/static/v1?label=&message={tr}&color={c})".format(tr = tr, c = color[tr][1:] if tr in color else "388d40"), score))
                 return '\n'.join(strs)
             else:
                 return 'This catagory is empty for now.'
